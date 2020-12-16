015609903aaad4901151badfbe312790a1893ae8
richard-martens <richard.martens@outlook.com>
2020-12-16 22:33:57 +0100

    4 files modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-import "./style.css";
+import MainMenu from "./MainMenu";
 
 export default function App() {
   return (
     <div>
+      <MainMenu />
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
--- src/MainMenu.js
+++ src/MainMenu.js
@@ -10,17 +10,12 @@ export default function App() {
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="/">Home</Nav.Link>
-      <Nav.Link href="#link"> <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
       <Nav.Link href="/">Startseite</Nav.Link>
       <Nav.Link href="/galery">Galerie</Nav.Link>
       <Nav.Link href="/Order">Bestellen</Nav.Link>
       <Nav.Link href="/about">Über mich</Nav.Link>
       <Nav.Link href="/contact">Kontakt</Nav.Link>
+      </Nav>
        </Navbar.Collapse>
 </Navbar>
 </Col>
